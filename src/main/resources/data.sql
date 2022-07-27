b01149065deb4d995a6c4505de3d0d608e35f818
fetsiura <fetsiura@gmail.com>
2022-07-27 12:27:07 +0200

    added third user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,5 @@ INSERT INTO USER (id, login,email,password,enabled) VALUES (1, 'linuxgt','linuxg
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO USER (id, login,email,password,enabled) VALUES (2, 'jaro','jaro@gmail.com','$2a$12$64Qtds/2pEUfR8TIbSHWtunZtYQAxm51UBd6Pp026L3cQTNg/8BRi',1);
 INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO USER (id, login,email,password,enabled) VALUES (3, 'julia','julia@gmail.com','$2a$12$64Qtds/2pEUfR8TIbSHWtunZtYQAxm51UBd6Pp026L3cQTNg/8BRi',1);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 1);
