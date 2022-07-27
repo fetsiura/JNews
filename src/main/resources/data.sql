1125f6ef7647fb79837ba6566f02d4f74b0b1ae6
fetsiura <fetsiura@gmail.com>
2022-07-27 10:19:31 +0200

    added second user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO Role (role_id, role) VALUES (1, 'ROLE_USER');
 INSERT INTO USER (id, login,email,password,enabled) VALUES (1, 'linuxgt','linuxgt@gmail.com','$2a$12$64Qtds/2pEUfR8TIbSHWtunZtYQAxm51UBd6Pp026L3cQTNg/8BRi',1);
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO USER (id, login,email,password,enabled) VALUES (2, 'jaro','jaro@gmail.com','$2a$12$64Qtds/2pEUfR8TIbSHWtunZtYQAxm51UBd6Pp026L3cQTNg/8BRi',1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
